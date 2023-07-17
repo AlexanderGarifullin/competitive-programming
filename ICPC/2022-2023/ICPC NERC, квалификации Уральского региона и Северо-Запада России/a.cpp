87e1f1ee99ae4fe435ec6289dd3032617eb40b1f
AlexanderGarifullin <139747118+AlexanderGarifullin@users.noreply.github.com>
2023-07-17 21:27:17 +0500

    math, O(1)

---
+++ ICPC/2022-2023/ICPC NERC, квалификации Уральского региона и Северо-Запада России/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define en '\n'
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t, l;
+    cin >> l >> t;
+    cout << 2 * t - l;
+    return 0;
+}
