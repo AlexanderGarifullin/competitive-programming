8617ad7529329736a291a79f62671b4a0b2c57f5
AlexanderGarifullin <139747118+AlexanderGarifullin@users.noreply.github.com>
2023-07-21 22:24:03 +0500

    read, print, compare __int128 in c++

--- algos/data-types/int128/read-print-compare.cpp
+++ algos/data-types/int128/read-print-compare.cpp
@@ -3,7 +3,7 @@ __int128 read() {
     char ch = getchar();
     while (ch < '0' || ch > '9') {
         if (ch == '-') f = -1;
-        ch = getchar();
+        ch = getchar(); 
     }
     while (ch >= '0' && ch <= '9') {
         x = x * 10 + ch - '0';
