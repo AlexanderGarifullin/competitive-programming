3eb1a73115fb0620c8ef2bc23192537b1ec702a7
AlexanderGarifullin <139747118+AlexanderGarifullin@users.noreply.github.com>
2023-07-24 23:39:13 +0500

    next placement using next permutation, O(n^2)

---
+++ algos/combinatorics/placements/my-next-placement(using-perm)).cpp
@@ -0,0 +1,54 @@
+
+// add to placement all elements from permutation 1..n that don't use in placement in descending order
+// generate next permutation
+// take first k elements
+bool next_placement(int n, vector<int> &place, vector<long long> fact){
+    int k = int(place.size());
+    vector<bool> used(n + 1);
+    vector<int> permutation(n);
+    for (int i = k - 1; i >=0 ; --i) {
+        used[place[i]] = true;
+        permutation[i] = place[i];
+    }
+    int j = n - 1;
+    for (int i = 1; i <= n ; ++i) {
+        if (!used[i]) {
+            permutation[j--] = i;
+        }
+    }
+    bool can = next_permutation(permutation.begin(), permutation.end());
+    if (!can) return can;
+    for (int i = 0; i < k; ++i) {
+        place[i] = permutation[i];
+    }
+    return true;
+}
+
+i32 main() {
+    //  freopen("input.txt", "rt", stdin);
+    //  freopen("output.txt", "wt", stdout);
+
+
+    cin.tie(nullptr); ios_base::sync_with_stdio(false);
+
+
+    int n, k; cin >> n >> k;
+
+    vector<int> placement(k);
+    for (int i = 0; i < k; ++i) {
+       placement[i] = i + 1;
+    }
+
+    vector<long long> factorials(n);
+    bool next = true;
+    do {
+        for (int i = 0; i < placement.size(); ++i) {
+            cout << placement[i];
+            if (i != int(placement.size()) - 1) cout << ' ';
+        }
+        next = next_placement(n, placement, factorials);
+        if (next) cout <<'\n';
+    } while(next);
+
+    return 0;
+}
