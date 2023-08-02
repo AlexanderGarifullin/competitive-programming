2289101cdecf3190ffbb124cf0736dd67adc1fd6
AlexanderGarifullin <139747118+AlexanderGarifullin@users.noreply.github.com>
2023-08-02 17:39:10 +0500

    combination by number, O(n)

---
+++ acmp.ru/629.cpp
@@ -0,0 +1,198 @@
+#include <bits/stdc++.h>
+ 
+// #pragma GCC target ("avx2")
+// #pragma GCC optimize("O3")
+// #pragma GCC optimize("unroll-loops")
+ 
+ 
+#define en '\n'
+#define f first
+//#define s second
+//#define mp make_pair
+#define  isz(x) int((x).size())
+#define ins insert
+#define pb push_back
+#define eb emplace_back
+#define ft front()
+#define bk back()
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define sortlargetosmall(x) sort(all(x), greater<>())
+#define sortsmalltolarge(x) sort(all(x), less<>())
+#define fin(x) for (auto &it: x) cin >> it
+#define fout(x) for (auto &it: x) cout << it << ' '; cout << en
+#define finpairs(x) for (auto &it: x) cin >> it.first >> it.second
+#define foutpairs(x) for (auto &it: x) cout << it.first << ' '<<it.second << ' '; cout << en
+#define cno cout << "NO" << en
+#define cyes cout << "YES" << en
+#define PI acos(-1.0L)
+#define fac(x, col) for(auto x: col)
+#define facl(x, col) for(auto &x: col)
+#define forn(start, end) for(int i = start; i < end; ++i)
+#define cmo cout << -1 << en
+#define ifyn(x) x ? cyes : cno
+#define xout(x) cout << x << en
+#define xin(x) cin >> x
+#define nline cout << en
+#define cen cerr <<"\n"
+ 
+ 
+ 
+#ifndef __DEBUG_HPP__
+#define __DEBUG_HPP__
+// ---- ---- ---- ---- ---- ---- DEBUG LIBRARY ---- ---- ---- ---- ---- ----
+#define watch(...) debug && std::cerr << "{" << #__VA_ARGS__ << "} = " \
+    << std::make_tuple(__VA_ARGS__) << std::endl
+ 
+template<typename... X>
+std::ostream& operator<<(std::ostream& os, const std::pair<X...>& p)
+{ return os << std::make_tuple(std::get<0>(p), std::get<1>(p)); }
+ 
+template<std::size_t I = 0, typename FuncT, typename... Tp>
+inline typename std::enable_if<I == sizeof...(Tp), void>::type
+for_each_const(const std::tuple<Tp...> &, FuncT) { }
+ 
+template<std::size_t I = 0, typename FuncT, typename... Tp>
+inline typename std::enable_if<I < sizeof...(Tp), void>::type
+for_each_const(const std::tuple<Tp...>& t, FuncT f)
+{ f(std::get<I>(t)),for_each_const<I + 1, FuncT, Tp...>(t, f); }
+ 
+ 
+template<std::size_t I = 0, typename FuncT, typename... Tp>
+inline typename std::enable_if<I == sizeof...(Tp), void>::type
+for_each(std::tuple<Tp...> &, FuncT) { }
+ 
+template<std::size_t I = 0, typename FuncT, typename... Tp>
+inline typename std::enable_if<I < sizeof...(Tp), void>::type
+for_each(std::tuple<Tp...>& t, FuncT f)
+{ f(std::get<I>(t)); for_each<I + 1, FuncT, Tp...>(t, f); }
+ 
+struct Printer {
+    std::ostream& os; bool was{0};
+    Printer(std::ostream& os_) : os(os_) { }
+    template<typename X> void operator()(X x)
+    { os << (was?", ":(was=1,"")) << x; }
+};
+ 
+template<typename... X>
+std::ostream& operator<<(std::ostream& os, const std::tuple<X...>& t)
+{ return os << "{", for_each_const(t, Printer(os)), os << "}"; }
+ 
+template<typename Iterator>
+std::ostream& print(std::ostream& os, Iterator begin, Iterator end)
+{ return os << "{", std::for_each(begin,end,Printer(os)), os << "}"; }
+ 
+#define OUTPUT(container) template<typename X, typename... T>           \
+std::ostream& operator<<(std::ostream& os, const container<X,T...>& c)  \
+{ return print(os, all(c)); }
+OUTPUT(std::vector) OUTPUT(std::list) OUTPUT(std::deque)
+OUTPUT(std::set) OUTPUT(std::unordered_set)
+OUTPUT(std::multiset) OUTPUT(std::unordered_multiset)
+OUTPUT(std::map) OUTPUT(std::multimap) OUTPUT(std::unordered_map)
+#undef RANGE_OUTPUT
+ 
+#define OUTPUT2(container, get, pop) template<typename X, typename... T> \
+std::ostream& operator<<(std::ostream& os, container<X,T...> c) {       \
+    std::vector<X> v(c.size());                                         \
+    for (unsigned i = 0; i != v.size(); v[i++] = c.get(),c.pop());      \
+    return os << v; }
+OUTPUT2(std::queue,front,pop)
+OUTPUT2(std::stack,top,pop)
+OUTPUT2(std::priority_queue,top,pop)
+#undef OUTPUT
+ 
+const int debug = 1;
+#endif // __DEBUG_HPP__
+ 
+using namespace std;
+ 
+ 
+using ll =  long long;
+using ld  = long double;
+using i64 = int64_t;
+using i32 = int32_t;
+using str = string;
+// pairs
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using graph = vector<set<int>>;
+ 
+template<typename T>
+using V = vector<T>;
+using vi = V<int>;
+using vl = V<ll>;
+using vpii = V<pii>;
+using vpll = V<pll>;
+using vb = V<bool>;
+using vc = V<char>;
+using vs = V<string>;
+using vld = V<ld>;
+ 
+using vvc = V<vc>;
+using vvb = V<vb>;
+using vvi = V<vi>;
+using vvl = V<vl>;
+using vvs = V<vs>;
+using vvpii = V<vpii>;
+using vvpll = V<vpll>;
+using vvld = V<vld>;
+ 
+using vvvi = V<vvi>;
+using vvvl = V<vvl>;
+ 
+using ull = unsigned long long;
+ 
+ 
+vector<int> get_combination( int n, int k, long long p, vector<vector<unsigned long long>> C){
+    vector<int> combination(k);
+    int next = 1;
+    int pos = 0;
+    while(k > 0){
+        if (p < C[n - 1][k - 1]) {
+            combination[pos++] = next;
+            k--;
+        } else {
+            p -= C[n-1][k-1];
+        }
+        n--;
+        next++;
+    }
+    return combination;
+}
+ 
+ 
+void fill_c(vector<vector<unsigned long long>> &C, int n){
+    for (int i = 0; i <= n; ++i) {
+        C[i][0]=1;
+    }
+    for (int i = 1; i <=n; ++i) {
+        for (int j = 1; j < i+1; ++j) {
+            C[i][j] = C[i-1][j] + C[i-1][j-1];
+        }
+    }
+}
+ 
+i32 main() {
+   //  freopen("comb.in", "rt", stdin);
+   //  freopen("comb.out", "wt", stdout);
+ 
+ 
+ 
+    cin.tie(nullptr); ios_base::sync_with_stdio(false);
+ 
+    int n, k; cin >> n >> k;
+    unsigned long long p; cin >> p;
+ 
+    vector<vector<unsigned long long>> C (n + 1, vector<unsigned long long> (n + 1));
+    fill_c(C, n);
+ 
+    vector<int> comb = get_combination(n, k ,p - 1, C);
+    map<int,char> alphabet;
+ 
+    for (int i = 0; i < 26; ++i) {
+        alphabet[i+1]='a'+i;
+    }
+ 
+    for (const int x : comb) cout << alphabet[x];
+}
