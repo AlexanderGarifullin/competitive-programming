d1cc64ccfc4f5da6074c53331fdeb39c97ac3fda
AlexanderGarifullin <139747118+AlexanderGarifullin@users.noreply.github.com>
2023-10-11 15:32:17 +0500

    implementation

---
+++ Codeforces/Rounds/Div-3/839/1772A.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#define en '\n'
+#define f first
+//#define s second
+#define mp make_pair
+#define  isz(x) int((x).size())
+#define ins insert
+#define pb push_back
+#define eb emplace_back
+#define ft front()
+#define bk back()
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define sor(x) sort(all(x))
+
+using namespace std;
+
+using ll =  long long;
+using ld  = long double;
+using str = string;
+// pairs
+using pi = pair<int,int>;
+using pl = pair<ll,ll>;
+
+template<typename T>
+using V = vector<T>;
+using vi = V<int>;
+using vl = V<ll>;
+using vpi = V<pi>;
+using vpl = V<pl>;
+using vb = V<bool>;
+
+using vvi = V<vi>;
+
+
+bool solve()
+{
+    int n,m,k;
+    cin >> n >> m >> k;
+    vi a(m);
+    for (int i = 0; i <m ; ++i) {
+        cin >> a[i];
+    }
+    if (m < k)
+        return false;
+
+    return true;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while(t--) {
+        string s;
+        cin >> s;
+        cout << (s[0]-'0') + (s[2]-'0') << en;
+    }
+
+}
