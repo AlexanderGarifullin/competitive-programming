b5c050d22b9fa037e610ce08c531845ff57e2762
AlexanderGarifullin <139747118+AlexanderGarifullin@users.noreply.github.com>
2024-01-15 08:08:31 +0500

    dp, graph, simplementation, shortest paths

---
+++ Codeforces/Rounds/Div-3/644/1360E.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define en '\n'
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        bool fl = 0;
+        vector<string> m(n);
+        for (int i = 0; i < n; ++i) {
+            string s;
+            cin >> s;
+            m[i] = s;
+        }
+        for (int i = 0; i < n- 1 && !fl; ++i) {
+            for (int j = 0; j < n- 1 && !fl; ++j) {
+                if (m[i][j] == '1' && m[i][j+1] != '1' &&  m[i+1][j] != '1')
+                {
+                   fl = 1;
+                   break;
+                }
+            }
+        }
+        if (fl)
+            cout << "NO" << en;
+        else
+            cout << "YES" << en;
+    }
+    return 0;
+}
