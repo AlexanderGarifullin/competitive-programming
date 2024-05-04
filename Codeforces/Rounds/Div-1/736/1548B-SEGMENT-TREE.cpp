0549e802056ace7201e64e93ce829a231a347131
AlexanderGarifullin <139747118+AlexanderGarifullin@users.noreply.github.com>
2024-05-04 17:40:12 +0500

    binary search, data structures, divide and conquer, math, number theory, two pointers

---
+++ Codeforces/Rounds/Div-1/736/1548B-SEGMENT-TREE.cpp
@@ -0,0 +1,186 @@
+#include <bits/stdc++.h>
+
+// #pragma GCC target ("avx2")
+// #pragma GCC optimize("O3")
+// #pragma GCC optimize("unroll-loops")
+
+
+#define en '\n'
+//#define mp make_pair
+#define  isz(x) int((x).size())
+#define ins insert
+#define pb push_back
+#define eb emplace_back
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define sortlargetosmall(x) sort(all(x), greater<>())
+#define sortsmalltolarge(x) sort(all(x), less<>())
+#define fin(x) for (auto &it: x) cin >> it
+#define fout(x) for (auto &it: x) cout << it << ' '; cout << en
+#define finpairs(x) for (auto &it: x) cin >> it.first >> it.second
+#define foutpairs(x) for (auto &it: x) cout << it.first << ' '<<it.second << ' '; cout << en
+#define cno cout << "NO" << en
+#define cyes cout << "YES" << en
+#define PI acos(-1.0L)
+#define forn(start, end) for(int i = start; i < end; ++i)
+#define cmo cout << -1 << en
+#define ifyn(x) x ? cyes : cno
+#define nline cout << en
+
+
+
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+using ll =  long long;
+using ld  = long double;
+using i64 = int64_t;
+using i32 = int32_t;
+using str = string;
+// pairs
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using graph = vector<set<int>>;
+
+template<typename T>
+using V = vector<T>;
+using vi = V<int>;
+using vl = V<ll>;
+using vpii = V<pii>;
+using vpll = V<pll>;
+using vb = V<bool>;
+using vc = V<char>;
+using vs = V<string>;
+using vld = V<ld>;
+
+using vvc = V<vc>;
+using vvb = V<vb>;
+using vvi = V<vi>;
+using vvl = V<vl>;
+using vvs = V<vs>;
+using vvpii = V<vpii>;
+using vvpll = V<vpll>;
+using vvld = V<vld>;
+
+using vvvi = V<vvi>;
+using vvvl = V<vvl>;
+
+using ull = unsigned long long;
+
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+template<typename T>
+using pqsmalleststart = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using pqmaximumstart = priority_queue<T, vector<T>, less<T>>;
+
+template<typename C> void reuniq(C& c) { c.erase(unique(all(c)), end(c));}
+
+
+void print(__int128 x) {
+    if (x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if (x > 9) print(x / 10);
+    putchar(x % 10 + '0');
+}
+
+
+
+int treeSize;
+vl segTree,dif;
+
+void build_tree(int v, int left, int right) {
+    if (left+1==right) {
+        segTree[v] = dif[left];
+    } else {
+        int mid = (left+right)/2;
+        int v_left = 2*v;
+        int v_right = v_left+1;
+        build_tree(v_left, left, mid);
+        build_tree(v_right, mid, right);
+        segTree[v] = __gcd(segTree[v_left], segTree[v_right]);
+    }
+}
+
+void build(int n){
+    treeSize = n;
+    segTree = vl(treeSize<<2);
+    build_tree(1, 0, treeSize);
+}
+
+int start_, end_;
+ll curValue;
+
+void get_tree(int v, int left, int right) {
+    if (start_ <= left && right <= end_) {
+        curValue = __gcd(curValue, segTree[v]);
+    } else {
+        int mid = (left+right)/2;
+        int v_left=2*v,v_right =v_left+1;
+        if (start_ < mid) get_tree(v_left, left, mid);
+        if (mid < end_) get_tree(v_right, mid, right);
+    }
+}
+
+ll get(int left, int right){
+    start_ = left;
+    end_ = right+1;
+    curValue = dif[left];
+    get_tree(1, 0, treeSize);
+    return curValue;
+}
+
+i32 main() {
+//    freopen("input.txt", "rt", stdin);
+//    freopen("output.txt", "wt", stdout);
+
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    // cout.setf(ios::fixed); cout.precision(20);
+
+    int t_; cin >> t_; while(t_--) {
+        int n; cin >> n;
+        vl a(n);
+        fin(a);
+        if (n == 1) {
+            cout << 1 << en;
+        } else {
+            dif = {};
+            dif.reserve(n-1);
+            for (int i = 1; i < n; ++i) {
+                dif.pb(abs(a[i]-a[i-1]));
+            }
+
+            build(isz(dif));
+
+            function<bool(int, int)> ok = [&](int left, int right){
+                ll curGCD = get(left, right);
+                return curGCD > 1;
+            };
+
+            int best = 1;
+            for (int i = 0; i < isz(dif); ++i) {
+                int left = i;
+                int right = isz(dif);
+                while(left+1<right){
+                    int mid = (left+right)/2;
+                    if (ok(i, mid)) {
+                        left=mid;
+                    } else right = mid;
+                }
+                // left = max I with GCD > 0
+                ll curGCD = get(i, left);
+                if (curGCD > 1) best=max(best, 1 +  left - i + 1);
+            }
+
+            cout << best << en;
+        }
+    }
+}
