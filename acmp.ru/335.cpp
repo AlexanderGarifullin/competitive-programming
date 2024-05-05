ec1e20bfb75b6346ffacd0abc004b8c93737a055
AlexanderGarifullin <139747118+AlexanderGarifullin@users.noreply.github.com>
2024-05-05 22:03:02 +0500

    Трипростые числа

---
+++ acmp.ru/335.cpp
@@ -0,0 +1,131 @@
+#include <bits/stdc++.h>
+
+// #pragma GCC target ("avx2")
+// #pragma GCC optimize("O3")
+// #pragma GCC optimize("unroll-loops")
+
+
+#define en '\n'
+//#define mp make_pair
+#define  isz(x) int((x).size())
+#define ins insert
+#define pb push_back
+#define eb emplace_back
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define sortlargetosmall(x) sort(all(x), greater<>())
+#define sortsmalltolarge(x) sort(all(x), less<>())
+#define fin(x) for (auto &it: x) cin >> it
+#define fout(x) for (auto &it: x) cout << it << ' '; cout << en
+#define finpairs(x) for (auto &it: x) cin >> it.first >> it.second
+#define foutpairs(x) for (auto &it: x) cout << it.first << ' '<<it.second << ' '; cout << en
+#define cno cout << "NO" << en
+#define cyes cout << "YES" << en
+#define PI acos(-1.0L)
+#define forn(start, end) for(int i = start; i < end; ++i)
+#define cmo cout << -1 << en
+#define ifyn(x) x ? cyes : cno
+#define nline cout << en
+
+
+
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+using ll =  long long;
+using ld  = long double;
+using i64 = int64_t;
+using i32 = int32_t;
+using str = string;
+// pairs
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using graph = vector<set<int>>;
+
+template<typename T>
+using V = vector<T>;
+using vi = V<int>;
+using vl = V<ll>;
+using vpii = V<pii>;
+using vpll = V<pll>;
+using vb = V<bool>;
+using vc = V<char>;
+using vs = V<string>;
+using vld = V<ld>;
+
+using vvc = V<vc>;
+using vvb = V<vb>;
+using vvi = V<vi>;
+using vvl = V<vl>;
+using vvs = V<vs>;
+using vvpii = V<vpii>;
+using vvpll = V<vpll>;
+using vvld = V<vld>;
+
+using vvvi = V<vvi>;
+using vvvl = V<vvl>;
+
+using ull = unsigned long long;
+
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+template<typename T>
+using pqsmalleststart = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using pqmaximumstart = priority_queue<T, vector<T>, less<T>>;
+
+template<typename C> void reuniq(C& c) { c.erase(unique(all(c)), end(c));}
+
+
+
+i32 main() {
+//    freopen("input.txt", "rt", stdin);
+//    freopen("output.txt", "wt", stdout);
+
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    // cout.setf(ios::fixed); cout.precision(20);
+
+
+    vb prime(1000,true);
+    prime[0]=prime[1]=false;
+    for (int i = 2; i * i < isz(prime); ++i) {
+        if (prime[i]){
+            for (int j = i*i; j < isz(prime); j+=i) {
+                prime[j] = false;
+            }
+        }
+    }
+
+    ll mod = 1e9 + 9;
+    int n; cin >> n;
+
+    vl last(100,0);
+
+    for (int i = 100; i < 1000; ++i) {
+        last[i%100] += prime[i];
+    }
+
+    for (int i = 4; i <= n; ++i) {
+        vl pred = last;
+        fill(all(last), 0);
+        for (int j = 100; j < 1000; ++j) {
+            if (prime[j]) {
+                last[j%100] += pred[j/10];
+                last[j%100] %= mod;
+            }
+        }
+    }
+
+    ll res = 0;
+    for (int i = 0; i < 100; ++i) {
+        res += last[i];
+        res %= mod;
+    }
+    cout << res;
+}
