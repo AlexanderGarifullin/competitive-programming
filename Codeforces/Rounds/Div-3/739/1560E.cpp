fc8c34f692d2bb54e303d6a9997e8cdddeede747
AlexanderGarifullin <139747118+AlexanderGarifullin@users.noreply.github.com>
2024-05-10 19:20:53 +0500

    binary search, implementation, sortings, strings

---
+++ Codeforces/Rounds/Div-3/739/1560E.cpp
@@ -0,0 +1,142 @@
+#include <bits/stdc++.h>
+
+// #pragma GCC target ("avx2")
+// #pragma GCC optimize("O3")
+// #pragma GCC optimize("unroll-loops")
+
+
+#define en '\n'
+//#define mp make_pair
+#define  isz(x) int((x).size())
+#define ins insert
+#define pb push_back
+#define eb emplace_back
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define sortlargetosmall(x) sort(all(x), greater<>())
+#define sortsmalltolarge(x) sort(all(x), less<>())
+#define fin(x) for (auto &it: x) cin >> it
+#define fout(x) for (auto &it: x) cout << it << ' '; cout << en
+#define finpairs(x) for (auto &it: x) cin >> it.first >> it.second
+#define foutpairs(x) for (auto &it: x) cout << it.first << ' '<<it.second << ' '; cout << en
+#define cno cout << "NO" << en
+#define cyes cout << "YES" << en
+#define PI acos(-1.0L)
+#define forn(start, end) for(int i = start; i < end; ++i)
+#define cmo cout << -1 << en
+#define ifyn(x) x ? cyes : cno
+#define nline cout << en
+
+
+
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+using ll =  long long;
+using ld  = long double;
+using i64 = int64_t;
+using i32 = int32_t;
+using str = string;
+// pairs
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using graph = vector<set<int>>;
+
+template<typename T>
+using V = vector<T>;
+using vi = V<int>;
+using vl = V<ll>;
+using vpii = V<pii>;
+using vpll = V<pll>;
+using vb = V<bool>;
+using vc = V<char>;
+using vs = V<string>;
+using vld = V<ld>;
+
+using vvc = V<vc>;
+using vvb = V<vb>;
+using vvi = V<vi>;
+using vvl = V<vl>;
+using vvs = V<vs>;
+using vvpii = V<vpii>;
+using vvpll = V<vpll>;
+using vvld = V<vld>;
+
+using vvvi = V<vvi>;
+using vvvl = V<vvl>;
+
+using ull = unsigned long long;
+
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+template<typename T>
+using pqsmalleststart = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using pqmaximumstart = priority_queue<T, vector<T>, less<T>>;
+
+template<typename C> void reuniq(C& c) { c.erase(unique(all(c)), end(c));}
+
+string check(str s, str order) {
+    str t = s;
+    for (char c : order) {
+        str cur;
+        for (char d : s) {
+            if (d != c) {
+                cur += d;
+                t += d;
+            }
+        }
+        s = cur;
+    }
+    return t;
+}
+
+i32 main() {
+//    freopen("input.txt", "rt", stdin);
+//    freopen("output.txt", "wt", stdout);
+
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    // cout.setf(ios::fixed); cout.precision(20);
+
+    int t_; cin >> t_; while(t_--) {
+        str t;
+        cin >> t;
+        int n = isz(t);
+        str order;
+        vb use(26);
+        vi cnt(26);
+        for (int i = n - 1; i >= 0; --i) {
+            if (!use[t[i] - 'a']) {
+                use[t[i] - 'a'] = true;
+                order += t[i];
+            }
+            cnt[t[i] - 'a']++;
+        }
+        reverse(all(order));
+        vi cnts(26);
+        bool ok = 1;
+        for (int i = 0; i < isz(order); ++i) {
+            if (cnt[order[i] - 'a'] % (i + 1) != 0) {
+                ok = 0;
+                break;
+            }
+            cnts[order[i] - 'a'] = cnt[order[i] - 'a'] / (i + 1);
+        }
+        if (!ok) {
+            cmo;
+            continue;
+        }
+        str s = t.substr(0, accumulate(all(cnts), 0));
+        if (t == check(s, order)) {
+            cout << s << ' ' << order << en;
+        } else {
+            cmo;
+        }
+    }
+}
