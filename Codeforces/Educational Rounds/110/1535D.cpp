f634764dafa040721d1d3a9b8a5077794aa3fe65
AlexanderGarifullin <139747118+AlexanderGarifullin@users.noreply.github.com>
2024-05-11 20:44:44 +0500

    data structures, dfs and similar, dp, implementation, trees

---
+++ Codeforces/Educational Rounds/110/1535D.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+
+// #pragma GCC target ("avx2")
+// #pragma GCC optimize("O3")
+// #pragma GCC optimize("unroll-loops")
+
+
+#define en '\n'
+//#define mp make_pair
+#define  isz(x) int((x).size())
+#define ins insert
+#define pb push_back
+#define eb emplace_back
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define sortlargetosmall(x) sort(all(x), greater<>())
+#define sortsmalltolarge(x) sort(all(x), less<>())
+#define fin(x) for (auto &it: x) cin >> it
+#define fout(x) for (auto &it: x) cout << it << ' '; cout << en
+#define finpairs(x) for (auto &it: x) cin >> it.first >> it.second
+#define foutpairs(x) for (auto &it: x) cout << it.first << ' '<<it.second << ' '; cout << en
+#define cno cout << "NO" << en
+#define cyes cout << "YES" << en
+#define PI acos(-1.0L)
+#define forn(start, end) for(int i = start; i < end; ++i)
+#define cmo cout << -1 << en
+#define ifyn(x) x ? cyes : cno
+#define nline cout << en
+
+
+
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+using ll =  long long;
+using ld  = long double;
+using i64 = int64_t;
+using i32 = int32_t;
+using str = string;
+// pairs
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using graph = vector<set<int>>;
+
+template<typename T>
+using V = vector<T>;
+using vi = V<int>;
+using vl = V<ll>;
+using vpii = V<pii>;
+using vpll = V<pll>;
+using vb = V<bool>;
+using vc = V<char>;
+using vs = V<string>;
+using vld = V<ld>;
+
+using vvc = V<vc>;
+using vvb = V<vb>;
+using vvi = V<vi>;
+using vvl = V<vl>;
+using vvs = V<vs>;
+using vvpii = V<vpii>;
+using vvpll = V<vpll>;
+using vvld = V<vld>;
+
+using vvvi = V<vvi>;
+using vvvl = V<vvl>;
+
+using ull = unsigned long long;
+
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+template<typename T>
+using pqsmalleststart = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using pqmaximumstart = priority_queue<T, vector<T>, less<T>>;
+
+template<typename C> void reuniq(C& c) { c.erase(unique(all(c)), end(c));}
+
+signed main() {
+//    freopen("input.txt", "rt", stdin);
+//    freopen("output.txt", "wt", stdout);
+
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    // cout.setf(ios::fixed); cout.precision(20);
+
+    int k,n; cin >> k;
+    str s; cin >> s;
+    n=(1<<k);
+    vi cnt(2*n, 1);
+    reverse(all(s));
+
+    function<void(int)> upd = [&](int pos){
+        if (s[pos] == '?') {
+            cnt[pos] = cnt[pos*2+2] + cnt[pos*2+1];
+        } else if (s[pos] == '0') {
+            cnt[pos] = cnt[pos*2+2];
+        } else {
+            cnt[pos] = cnt[pos*2+1];
+        }
+    };
+
+    for (int i = n-2; i >=0 ; --i) {
+        upd(i);
+    }
+
+    int q; cin >> q; while(q--) {
+        int pos; cin >> pos;
+        char c; cin >> c;
+        pos = n - 1 - pos;
+        s[pos] = c;
+        while(pos) {
+            upd(pos);
+            pos = (pos - 1) / 2;
+        } 
+        upd(0);
+        cout << cnt[0] << en;
+    }
+}
