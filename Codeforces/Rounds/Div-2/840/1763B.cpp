2c76356b193805be4ff1f6381ac66361ae41cb54
AlexanderGarifullin <139747118+AlexanderGarifullin@users.noreply.github.com>
2024-08-24 19:53:28 +0500

    binary search, brute force, data structures, implementation, math, sortings

---
+++ Codeforces/Rounds/Div-2/840/1763B.cpp
@@ -0,0 +1,197 @@
+#include <bits/stdc++.h>
+#define en '\n'
+#define f first
+//#define s second
+#define mp make_pair
+#define  isz(x) int((x).size())
+#define ins insert
+#define pb push_back
+#define eb emplace_back
+#define ft front()
+#define bk back()
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define sor(x) sort(all(x))
+#define for_cin(x) for (auto &it: x) cin >> it
+#define for_out(x) for (auto &it: x) cout << it << ' '; cout << en;
+#define cNO cout << "NO" << en;
+#define cYES cout << "YES" << en;
+
+#define watch(x) std::cout << #x << " = " << x << std::endl;
+#define debug(...) out (#__VA_ARGS__, __VA_ARGS__)
+
+
+
+using namespace std;
+
+using ll =  long long;
+using ld  = long double;
+using str = string;
+// pairs
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+
+template<typename T>
+using V = vector<T>;
+using vi = V<int>;
+using vl = V<ll>;
+using vpii = V<pii>;
+using vpll = V<pll>;
+using vb = V<bool>;
+
+using vvi = V<vi>;
+
+using ull = unsigned long long;
+
+class Timer {
+    std::chrono::time_point<std::chrono::steady_clock> timePoint;
+    size_t value;
+public:
+    void start() { timePoint = std::chrono::steady_clock::now(); }
+    void finish() {
+        auto curr = std::chrono::steady_clock::now();
+        auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(curr - timePoint);
+        value = elapsed.count();
+    }
+    size_t operator()() const { return value; }
+};
+
+
+string to_string(const string& s) {
+    return '"' + s + '"';
+}
+
+string to_string(const char* s) {
+    return to_string((string)s);
+}
+
+string to_string(bool b) {
+    return (b ? "true" : "false");
+}
+
+template <typename A>
+string to_string(A v){
+    return empty(v) ? "{}": accumulate(++cbegin(v), cend(v), "{ " + to_string(*cbegin(v)),
+                                       [](auto l, auto r){return l+", "+ to_string(r);}) + " }";
+}
+
+void out(string) { cerr << endl;}
+
+template<typename Head, typename ... Tail>
+void out(string format, Head H, Tail... T){
+    if (format.find(',') == string::npos) {
+        cerr << format << " ::";
+    } else {
+        cerr << string(format.begin(), format.begin() + format.find(',')) << " ::";
+        format = string(format.begin() + format.find(',') + 2,format.end());
+    }
+    cerr << " " << to_string(H) << endl;
+    out (format, T...);
+}
+
+// Шаблон на поиск максимума среди нескольких элементов.
+template<typename T>
+T Max(T a)
+{
+    return a;
+}
+
+template<typename T, typename... Package>
+T Max (T x, Package... p)
+{
+    return std::max(x, Max(p...));
+}
+
+// Шаблон на поиск минимума среди нескольких элементов.
+template<typename T>
+T Min(T a)
+{
+    return a;
+}
+
+template<typename T, typename... Package>
+T Min (T x, Package... p)
+{
+    return std::min(x, Min(p...));
+}
+
+Timer timer;
+
+ll binpow (ll a, ll n) {
+    if (n == 0)
+        return 1ll;
+    if (a == 1) return 1;
+    if (n % 2 == 1)
+        return binpow (a, n-1) * a;
+    else {
+        ll b = binpow (a, n/2);
+        return b * b;
+    }
+}
+
+
+ll binpow(ll a, ll n, ll mod) {
+    ll res = 1 % mod;
+    while (n > 0) {
+        if (n % 2 == 1)
+            res = res * a % mod;
+        a = a * a % mod;
+        n >>= 1;
+    }
+    return res % mod;
+}
+
+ll gcd(ll a,ll b){
+    return b?gcd(b,a%b):a;
+}
+
+
+// Обратное число a^(-1) = a^(p-2)
+// a^(-1) = binpow(a, mod - 2, mod)
+// 1) q, q^2, ... , q^k, 0, 0 ... - Необратимый элемент. binpow(q, 10^18, mod) == 0. q^(k+1) % mod == 0. Нельзя найти q^(-1)
+// 2) q, q^2, ... , q^k, 1, q,  q^2, ... , q^k, ... Существует обратный элемент. Тогда => a^(-1) = a^(p-2) (mod p)
+// a^(-1) = binpow(a, mod - 2, mod) - Если mod - простое.
+// a^(-1) = binpow(a, f(m) - 1, m) - В общем случае (Если m не простое).  f(m) - Функция Эйлера - кол-во чисел от 1 до m - 1, где их НОД с m = 1
+// Функция эйлера: f(p1^k1, p2^k2, ... , pn^kn) =  (p1^k1 - p1^(k1-1)) * (p2^k2 - p2^(k2-1)) (pn^kn - pn^(kn-1));
+
+bool solve(){
+    int n, k; cin >> n >> k;
+    vpii  v(n);
+    for (int i = 0; i <n ; ++i) {
+        int h; cin >> h;
+        v[i].first = h;
+    }
+    for (int i = 0; i < n; ++i) {
+        int p; cin >> p;
+        v[i].second = p;
+    }
+    sort(all(v), [](pii & p1,pii & p2 ){return p1.second < p2.second;});
+
+    ll sum_damage = k;
+    for (auto &[hp, damage]: v){
+        while (hp > sum_damage){
+            k -= damage;
+            sum_damage += k;
+            if (k <= 0) return false;
+        }
+    }
+    return  true;
+}
+
+int main() {
+   ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    timer.start();
+
+    int q;
+    cin >> q;
+    while (q--){
+        if (solve()) cYES
+        else cNO;
+    }
+
+
+    timer.finish();
+    cerr << en << string(20,'-') << en <<"runtime = " << timer() << " ms" << en;
+    return 0;
+}
