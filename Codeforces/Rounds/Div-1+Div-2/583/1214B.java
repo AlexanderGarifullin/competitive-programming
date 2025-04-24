a62add173d572aeb110678ecbd9ca6caac6ba4c1
Alexander Garifullin <139747118+AlexanderGarifullin@users.noreply.github.com>
2025-04-24 10:51:08 +0500

    brute force, math, O(1)

---
+++ Codeforces/Rounds/Div-1+Div-2/583/1214B.java
@@ -0,0 +1,31 @@
+import javax.crypto.spec.PSource;
+import java.util.*;
+
+public class Main {
+    static Scanner cin = new Scanner(System.in);
+
+    public static <T extends Comparable<? super T>> boolean isSorted(List<T> list) {
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i-1).compareTo(list.get(i)) > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static void solve() {
+        int b = cin.nextInt();
+        int g = cin.nextInt();
+        int n = cin.nextInt();
+        System.out.println(Math.min(g, n) - (n - Math.min(b, n)) + 1);
+    }
+
+
+    public static void main(String[] args) {
+//        int t = cin.nextInt();
+        int t = 1;
+        while (t-- > 0) {
+            solve();
+        }
+    }
+}
