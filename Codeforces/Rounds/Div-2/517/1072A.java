afbd49cc7b493102f5172f3390531f0c9d87af68
Alexander Garifullin <139747118+AlexanderGarifullin@users.noreply.github.com>
2025-04-30 10:58:48 +0500

    implementation, math

---
+++ Codeforces/Rounds/Div-2/517/1072A.java
@@ -0,0 +1,64 @@
+import java.security.PublicKey;
+import java.util.*;
+
+public class Main {
+    static Scanner cin = new Scanner(System.in);
+
+    public static <T extends Comparable<? super T>> boolean isSorted(List<T> list) {
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i-1).compareTo(list.get(i)) > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static long gcd(long a, long b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    public static long lcm(long a, long b) {
+        return a * b / gcd(a,b);
+    }
+
+    static int getHash(int r, int c, int n) {
+        return r * n + c;
+    }
+
+    static class Pair<F,S> {
+        F first;
+        S second;
+
+        Pair(F first, S second) {
+            this.first = first;
+            this.second = second;
+        }
+
+        static <F,S> Pair<F,S> of(F first, S second) {
+            return new Pair<>(first, second);
+        }
+    }
+
+    static void solve() {
+        int w = cin.nextInt();
+        int h = cin.nextInt();
+        int k = cin.nextInt();
+        long ans = 0;
+        for (int i = 1; i <= k; i++) {
+            long x1 =   Math.max(0, (w - 4 * (i - 1) - 2) * (h - 4 * (i - 1) - 2));
+            long x2 = (w - 4 * (i - 1)) * (h - 4 * (i - 1)) ;
+            ans += x2-x1;
+        }
+        System.out.println(ans);
+    }
+
+
+    public static void main(String[] args) {
+//        int t = cin.nextInt();
+        int t = 1;
+        while (t-- > 0) {
+            solve();
+        }
+    }
+}
