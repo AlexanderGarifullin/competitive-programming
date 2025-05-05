9dd32c94cd6f0c2500bbfb3d558d96d451f4de15
Alexander Garifullin <139747118+AlexanderGarifullin@users.noreply.github.com>
2025-05-05 09:27:24 +0500

    greedy, math, sortings

---
+++ Codeforces/Rounds/594/1248B.java
@@ -0,0 +1,68 @@
+import java.security.PublicKey;
+import java.util.*;
+
+public class Main {
+    static Scanner cin = new Scanner(System.in);
+
+    public static <T extends Comparable<? super T>> boolean isSorted(List<T> list) {
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i-1).compareTo(list.get(i)) > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static long gcd(long a, long b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    public static long lcm(long a, long b) {
+        return a * b / gcd(a,b);
+    }
+
+    static int getHash(int r, int c, int n) {
+        return r * n + c;
+    }
+
+    static class Pair<F,S> {
+        F first;
+        S second;
+
+        Pair(F first, S second) {
+            this.first = first;
+            this.second = second;
+        }
+
+        static <F,S> Pair<F,S> of(F first, S second) {
+            return new Pair<>(first, second);
+        }
+    }
+
+    static void solve() {
+        int n = cin.nextInt();
+        List<Integer> v = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int x = cin.nextInt();
+            v.add(x);
+        }
+        Collections.sort(v);
+        long sum1 =0, sum2=0;
+        for (int i = 0; i < n/2; i++) {
+            sum1 += v.get(i);
+        }
+        for (int i = n/2; i < n; i++) {
+            sum2 += v.get(i);
+        }
+        System.out.println(sum1 * sum1 + sum2 * sum2);
+    }
+
+    public static void main(String[] args) {
+//        int t = cin.nextInt();
+        int t = 1;
+        while (t-- > 0) {
+            solve();
+        }
+    }
+}
