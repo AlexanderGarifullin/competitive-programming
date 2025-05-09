9783e5cd16786ff0cf9ff8244b174a1c85db80c5
Alexander Garifullin <139747118+AlexanderGarifullin@users.noreply.github.com>
2025-05-09 18:23:56 +0500

    graphs, hashing, sortings

---
+++ Codeforces/Rounds/Div-1/109/154C.java
@@ -0,0 +1,148 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+
+public class Main {
+    private static final class FastScanner {
+        private final byte[] buf = new byte[1 << 16];
+        private int len = 0, ptr = 0;
+        private final InputStream in;
+
+        FastScanner(InputStream in) {
+            this.in = in;
+        }
+
+        private int readByte() throws java.io.IOException {
+            if (ptr >= len) {
+                len = in.read(buf);
+                ptr = 0;
+                if (len <= 0) return -1;
+            }
+            return buf[ptr++];
+        }
+
+        int nextInt() throws java.io.IOException {
+            int c, sign = 1, val = 0;
+            do { c = readByte(); } while (c <= ' ' && c != -1);
+            if (c == '-') { sign = -1; c = readByte(); }
+            while (c > ' ' && c != -1) {
+                val = val * 10 + (c - '0');
+                c = readByte();
+            }
+            return val * sign;
+        }
+
+        long nextLong() throws java.io.IOException {
+            long c, sign = 1, val = 0;
+            do { c = readByte(); } while (c <= ' ' && c != -1);
+            if (c == '-') { sign = -1; c = readByte(); }
+            while (c > ' ' && c != -1) {
+                val = val * 10 + (c - '0');
+                c = readByte();
+            }
+            return val * sign;
+        }
+
+        String next() throws java.io.IOException {
+            int c;
+            do {
+                c = readByte();
+            } while (c <= ' ' && c != -1);
+
+            if (c == -1) return null;
+
+            StringBuilder sb = new StringBuilder();
+            while (c > ' ' && c != -1) {
+                sb.append((char) c);
+                c = readByte();
+            }
+            return sb.toString();
+        }
+    }
+
+    static FastScanner cin = new FastScanner(System.in);
+
+    public static <T extends Comparable<? super T>> boolean isSorted(List<T> list) {
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i-1).compareTo(list.get(i)) > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static long gcd(long a, long b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    public static long lcm(long a, long b) {
+        return a * b / gcd(a,b);
+    }
+
+    private static final int MAX = 500_000 + 5;
+
+    private static int[] mobius = new int[MAX];
+    private static int[] cnt    = new int[MAX];
+    private static boolean[] onShelf;
+    private static int[][] divisors;
+    private static long answer = 0;
+
+    static class Pair<F,S> {
+        F first;
+        S second;
+
+        Pair(F first, S second) {
+            this.first = first;
+            this.second = second;
+        }
+
+        static <F,S> Pair<F,S> of(F first, S second) {
+            return new Pair<>(first, second);
+        }
+    }
+
+    static void solve() throws IOException {
+        int n = cin.nextInt();
+        int m = cin.nextInt();
+        long [] a = new long[n];
+        long [] xr = new long [n];
+        SplittableRandom rnd = new SplittableRandom(228);
+        for (int i = 0; i <n ; i++) {
+            a[i] = rnd.nextLong();
+        }
+        int [] U = new int[m];
+        int [] V = new int[m];
+
+        for (int i = 0; i < m; ++i) {
+            int u = cin.nextInt() - 1;
+            int v = cin.nextInt() - 1;
+            U[i] = u;
+            V[i] = v;
+            xr[u] ^= a[v];
+            xr[v] ^= a[u];
+        }
+        long ans = 0;
+        for (int i = 0; i < m; ++i) {
+            int u = U[i], v = V[i];
+            if ((xr[u] ^ xr[v]) == (a[u] ^ a[v])) ++ans;
+        }
+        Arrays.sort(xr);
+        for (int i = 0; i < n; ) {
+            int j = i + 1;
+            while (j < n && xr[i] == xr[j]) ++j;
+            long len = j - i;
+            ans += len * (len - 1) / 2;
+            i = j;
+        }
+        System.out.println(ans);
+    }
+
+    public static void main(String[] args) throws IOException {
+//        int t = cin.nextInt();
+        int t = 1;
+        while (t-- > 0) {
+            solve();
+        }
+    }
+}
