3edaa920d7303210cf0289cb77416f79808ef7cb
Alexander Garifullin <139747118+AlexanderGarifullin@users.noreply.github.com>
2025-05-13 15:21:53 +0500

    constructive algorithms

---
+++ Codeforces/Rounds/Div-2/1024/2102C.java
@@ -0,0 +1,146 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+
+public class Main {
+    private static final class FastScanner {
+        private final byte[] buf = new byte[1 << 16];
+        private int len = 0, ptr = 0;
+        private final InputStream in;
+
+        FastScanner(InputStream in) {
+            this.in = in;
+        }
+
+        private int readByte() throws java.io.IOException {
+            if (ptr >= len) {
+                len = in.read(buf);
+                ptr = 0;
+                if (len <= 0) return -1;
+            }
+            return buf[ptr++];
+        }
+
+        int nextInt() throws java.io.IOException {
+            int c, sign = 1, val = 0;
+            do { c = readByte(); } while (c <= ' ' && c != -1);
+            if (c == '-') { sign = -1; c = readByte(); }
+            while (c > ' ' && c != -1) {
+                val = val * 10 + (c - '0');
+                c = readByte();
+            }
+            return val * sign;
+        }
+
+        long nextLong() throws java.io.IOException {
+            long c, sign = 1, val = 0;
+            do { c = readByte(); } while (c <= ' ' && c != -1);
+            if (c == '-') { sign = -1; c = readByte(); }
+            while (c > ' ' && c != -1) {
+                val = val * 10 + (c - '0');
+                c = readByte();
+            }
+            return val * sign;
+        }
+
+        String next() throws java.io.IOException {
+            int c;
+            do {
+                c = readByte();
+            } while (c <= ' ' && c != -1);
+
+            if (c == -1) return null;
+
+            StringBuilder sb = new StringBuilder();
+            while (c > ' ' && c != -1) {
+                sb.append((char) c);
+                c = readByte();
+            }
+            return sb.toString();
+        }
+    }
+
+    static FastScanner cin = new FastScanner(System.in);
+
+    public static <T extends Comparable<? super T>> boolean isSorted(List<T> list) {
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i-1).compareTo(list.get(i)) > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static long gcd(long a, long b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    public static long lcm(long a, long b) {
+        return a * b / gcd(a,b);
+    }
+
+    private static final int MAX = 500_000 + 5;
+
+    private static int[] mobius = new int[MAX];
+    private static int[] cnt    = new int[MAX];
+    private static boolean[] onShelf;
+    private static int[][] divisors;
+    private static long answer = 0;
+
+    static class Pair<F,S> {
+        F first;
+        S second;
+
+        Pair(F first, S second) {
+            this.first = first;
+            this.second = second;
+        }
+
+        static <F,S> Pair<F,S> of(F first, S second) {
+            return new Pair<>(first, second);
+        }
+    }
+
+    static void solve() throws IOException {
+        int N = 510;
+        int a[][] = new int[N][N];
+        int n = cin.nextInt();
+        int i = n/2;
+        int j = n/2;
+        if (n % 2 == 0) {
+            i--;
+            j--;
+        }
+        int id = 2;
+        int t = 0;
+        int val = 0;
+        while(val < n * n) {
+            int sz = id / 2;
+            while(sz-->0) {
+                a[i][j] = val++;
+                if (t == 0) j++;
+                else if (t == 1) i++;
+                else if (t == 2) j--;
+                else if (t == 3) i--;
+            }
+            t = (t + 1) % 4;
+            id++;
+        }
+
+        for (int k = 0; k < n; k++) {
+            for (int l = 0; l < n; l++) {
+                System.out.print(a[k][l] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        int t = cin.nextInt();
+//        int t = 1;
+        while (t-- > 0) {
+            solve();
+        }
+    }
+}
