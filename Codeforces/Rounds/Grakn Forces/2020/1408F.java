5d5697dc1567b22296dd97eaab639d579d099cb1
Alexander Garifullin <139747118+AlexanderGarifullin@users.noreply.github.com>
2025-05-19 13:59:26 +0500

    constructive algorithms, divide and conquer

---
+++ Codeforces/Rounds/Grakn Forces/2020/1408F.java
@@ -0,0 +1,131 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+import java.util.function.BiConsumer;
+
+public class Main {
+    private static final class FastScanner {
+        private final byte[] buf = new byte[1 << 16];
+        private int len = 0, ptr = 0;
+        private final InputStream in;
+
+        FastScanner(InputStream in) {
+            this.in = in;
+        }
+
+        private int readByte() throws java.io.IOException {
+            if (ptr >= len) {
+                len = in.read(buf);
+                ptr = 0;
+                if (len <= 0) return -1;
+            }
+            return buf[ptr++];
+        }
+
+        int nextInt() throws java.io.IOException {
+            int c, sign = 1, val = 0;
+            do { c = readByte(); } while (c <= ' ' && c != -1);
+            if (c == '-') { sign = -1; c = readByte(); }
+            while (c > ' ' && c != -1) {
+                val = val * 10 + (c - '0');
+                c = readByte();
+            }
+            return val * sign;
+        }
+
+        long nextLong() throws java.io.IOException {
+            long c, sign = 1, val = 0;
+            do { c = readByte(); } while (c <= ' ' && c != -1);
+            if (c == '-') { sign = -1; c = readByte(); }
+            while (c > ' ' && c != -1) {
+                val = val * 10 + (c - '0');
+                c = readByte();
+            }
+            return val * sign;
+        }
+
+        String next() throws java.io.IOException {
+            int c;
+            do {
+                c = readByte();
+            } while (c <= ' ' && c != -1);
+
+            if (c == -1) return null;
+
+            StringBuilder sb = new StringBuilder();
+            while (c > ' ' && c != -1) {
+                sb.append((char) c);
+                c = readByte();
+            }
+            return sb.toString();
+        }
+    }
+
+    static FastScanner cin = new FastScanner(System.in);
+
+    public static <T extends Comparable<? super T>> boolean isSorted(List<T> list) {
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i-1).compareTo(list.get(i)) > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static long gcd(long a, long b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    public static long lcm(long a, long b) {
+        return a * b / gcd(a,b);
+    }
+
+    static long modPow(long base, int exp, int mod) {
+        long result = 1;
+        base %= mod;
+        while (exp > 0) {
+            if ((exp & 1) == 1)
+                result = result * base % mod;
+            base = base * base % mod;
+            exp >>= 1;
+        }
+        return result;
+    }
+
+    static void solve() throws IOException {
+        int n = cin.nextInt();
+
+        // log
+        int k = 31 - Integer.numberOfLeadingZeros(n);
+        int len = 1 << k;
+
+        List<int[]> ans = new ArrayList<>();
+
+        BiConsumer<Integer, Integer> work = (x, length) -> {
+            for (int t = 1; t < length; t *= 2) {
+                for (int i = 0; i < length; i += 2 * t) {
+                    for (int j = 0; j < t; ++j) {
+                        ans.add(new int[]{x + i + j + 1, x + i + j + t + 1});
+                    }
+                }
+            }
+        };
+
+        work.accept(0, len);
+        work.accept(n - len, len);
+
+        System.out.println(ans.size());
+        for (int[] pair : ans) {
+            System.out.println(pair[0] + " " + pair[1]);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+//        int t = cin.nextInt();
+        int t = 1;
+        while (t-- > 0) {
+            solve();
+        }
+    }
+}
