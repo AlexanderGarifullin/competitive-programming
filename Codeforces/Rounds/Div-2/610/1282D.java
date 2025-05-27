a22e96bc4fb6a87e5e74b4f4e2ed01640c1d1415
Alexander Garifullin <139747118+AlexanderGarifullin@users.noreply.github.com>
2025-05-27 14:05:35 +0500

    constructive algorithms, interactive, strings

---
+++ Codeforces/Rounds/Div-2/610/1282D.java
@@ -0,0 +1,146 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+import java.util.function.BiConsumer;
+
+public class Main {
+    private static final class FastScanner {
+        private final byte[] buf = new byte[1 << 16];
+        private int len = 0, ptr = 0;
+        private final InputStream in;
+
+        FastScanner(InputStream in) {
+            this.in = in;
+        }
+
+        private int readByte() throws java.io.IOException {
+            if (ptr >= len) {
+                len = in.read(buf);
+                ptr = 0;
+                if (len <= 0) return -1;
+            }
+            return buf[ptr++];
+        }
+
+        int nextInt() throws java.io.IOException {
+            int c, sign = 1, val = 0;
+            do { c = readByte(); } while (c <= ' ' && c != -1);
+            if (c == '-') { sign = -1; c = readByte(); }
+            while (c > ' ' && c != -1) {
+                val = val * 10 + (c - '0');
+                c = readByte();
+            }
+            return val * sign;
+        }
+
+        long nextLong() throws java.io.IOException {
+            long c, sign = 1, val = 0;
+            do { c = readByte(); } while (c <= ' ' && c != -1);
+            if (c == '-') { sign = -1; c = readByte(); }
+            while (c > ' ' && c != -1) {
+                val = val * 10 + (c - '0');
+                c = readByte();
+            }
+            return val * sign;
+        }
+
+        String next() throws java.io.IOException {
+            int c;
+            do {
+                c = readByte();
+            } while (c <= ' ' && c != -1);
+
+            if (c == -1) return null;
+
+            StringBuilder sb = new StringBuilder();
+            while (c > ' ' && c != -1) {
+                sb.append((char) c);
+                c = readByte();
+            }
+            return sb.toString();
+        }
+    }
+
+    static FastScanner cin = new FastScanner(System.in);
+
+    public static <T extends Comparable<? super T>> boolean isSorted(List<T> list) {
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i-1).compareTo(list.get(i)) > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static long gcd(long a, long b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    public static long lcm(long a, long b) {
+        return a * b / gcd(a,b);
+    }
+
+    static long modPow(long base, int exp, int mod) {
+        long result = 1;
+        base %= mod;
+        while (exp > 0) {
+            if ((exp & 1) == 1)
+                result = result * base % mod;
+            base = base * base % mod;
+            exp >>= 1;
+        }
+        return result;
+    }
+    static int query(String s) throws IOException {
+        System.out.println(s);
+        System.out.flush();
+        int response = cin.nextInt();
+        if (response == 0 || response == -1) {
+            System.exit(0);
+        }
+        return response;
+    }
+
+    static void solve() throws IOException {
+        final int MAX_LENGTH = 300;
+        int distA = query("a".repeat(MAX_LENGTH));
+        int distB = query("b".repeat(MAX_LENGTH));
+
+        int totalLength = 2 * MAX_LENGTH - (distA + distB);
+        int numA = MAX_LENGTH - distA;
+        int numB = totalLength - numA;
+
+        char[] guess = new char[totalLength];
+        for (int i = 0; i < totalLength; i++) {
+            guess[i] = 'a';
+        }
+
+        int remainingB = numB;
+
+        for (int i = 0; i < totalLength - 1; i++) {
+            guess[i] = 'b';
+            int newDist = query(new String(guess));
+            if (newDist > remainingB) {
+                guess[i] = 'a';
+            } else {
+                remainingB--;
+            }
+        }
+
+        if (remainingB == 1) {
+            guess[totalLength - 1] = 'b';
+        }
+
+        System.out.println(new String(guess));
+        System.out.flush();
+    }
+
+    public static void main(String[] args) throws IOException {
+//        int t = cin.nextInt();
+        int t = 1;
+        while (t-- > 0) {
+            solve();
+        }
+    }
+}
