056da4a8131e784e82a180beed5667ee80f8d00f
Alexander Garifullin <139747118+AlexanderGarifullin@users.noreply.github.com>
2025-06-07 09:37:47 +0500

    dp

---
+++ Codeforces/Rounds/Div-3/787/1675G.java
@@ -0,0 +1,140 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+import java.util.function.BiConsumer;
+
+public class Main {
+    private static final class FastScanner {
+        private final byte[] buf = new byte[1 << 16];
+        private int len = 0, ptr = 0;
+        private final InputStream in;
+
+        FastScanner(InputStream in) {
+            this.in = in;
+        }
+
+        private int readByte() throws java.io.IOException {
+            if (ptr >= len) {
+                len = in.read(buf);
+                ptr = 0;
+                if (len <= 0) return -1;
+            }
+            return buf[ptr++];
+        }
+
+        int nextInt() throws java.io.IOException {
+            int c, sign = 1, val = 0;
+            do { c = readByte(); } while (c <= ' ' && c != -1);
+            if (c == '-') { sign = -1; c = readByte(); }
+            while (c > ' ' && c != -1) {
+                val = val * 10 + (c - '0');
+                c = readByte();
+            }
+            return val * sign;
+        }
+
+        long nextLong() throws java.io.IOException {
+            long c, sign = 1, val = 0;
+            do { c = readByte(); } while (c <= ' ' && c != -1);
+            if (c == '-') { sign = -1; c = readByte(); }
+            while (c > ' ' && c != -1) {
+                val = val * 10 + (c - '0');
+                c = readByte();
+            }
+            return val * sign;
+        }
+
+        String next() throws java.io.IOException {
+            int c;
+            do {
+                c = readByte();
+            } while (c <= ' ' && c != -1);
+
+            if (c == -1) return null;
+
+            StringBuilder sb = new StringBuilder();
+            while (c > ' ' && c != -1) {
+                sb.append((char) c);
+                c = readByte();
+            }
+            return sb.toString();
+        }
+    }
+
+    static FastScanner cin = new FastScanner(System.in);
+
+    public static <T extends Comparable<? super T>> boolean isSorted(List<T> list) {
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i-1).compareTo(list.get(i)) > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static long gcd(long a, long b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    public static long lcm(long a, long b) {
+        return a * b / gcd(a,b);
+    }
+
+    static long modPow(long base, int exp, int mod) {
+        long result = 1;
+        base %= mod;
+        while (exp > 0) {
+            if ((exp & 1) == 1)
+                result = result * base % mod;
+            base = base * base % mod;
+            exp >>= 1;
+        }
+        return result;
+    }
+
+    static void solve() throws IOException {
+        int n = cin.nextInt();
+        int m = cin.nextInt();
+
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = cin.nextInt();
+        }
+
+        int[] s = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            s[i + 1] = s[i] + a[i];
+        }
+
+        final int INF = (int)1e9;
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 0; i <= n; i++) {
+            Arrays.fill(dp[i], INF);
+        }
+        dp[0][0] = 0;
+
+        for (int x = m; x >= 0; x--) {
+            for (int j = 0; j + x <= m; j++) {
+                for (int i = 0; i < n && i * x <= j; i++) {
+                    int nj = j + x;
+                    int cost = dp[i][j] + Math.abs(s[i] - j);
+                    if (cost < dp[i + 1][nj]) {
+                        dp[i + 1][nj] = cost;
+                    }
+                }
+            }
+        }
+
+        System.out.println(dp[n][m]);
+    }
+
+
+    public static void main(String[] args) throws IOException {
+//        int t = cin.nextInt();
+        int t = 1;
+        while (t-- > 0) {
+            solve();
+        }
+    }
+}
