1b96a4f6d9c1342de9e5c8a7a63ca46584e99913
Alexander Garifullin <139747118+AlexanderGarifullin@users.noreply.github.com>
2025-06-13 10:39:47 +0500

    dp, graphs, greedy, math, probabilities

---
+++ Codeforces/Rounds/Div-1/901/1874C.java
@@ -0,0 +1,147 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.BiConsumer;
+
+public class Main {
+    private static final class FastScanner {
+        private final byte[] buf = new byte[1 << 16];
+        private int len = 0, ptr = 0;
+        private final InputStream in;
+
+        FastScanner(InputStream in) {
+            this.in = in;
+        }
+
+        private int readByte() throws java.io.IOException {
+            if (ptr >= len) {
+                len = in.read(buf);
+                ptr = 0;
+                if (len <= 0) return -1;
+            }
+            return buf[ptr++];
+        }
+
+        int nextInt() throws java.io.IOException {
+            int c, sign = 1, val = 0;
+            do { c = readByte(); } while (c <= ' ' && c != -1);
+            if (c == '-') { sign = -1; c = readByte(); }
+            while (c > ' ' && c != -1) {
+                val = val * 10 + (c - '0');
+                c = readByte();
+            }
+            return val * sign;
+        }
+
+        long nextLong() throws java.io.IOException {
+            long c, sign = 1, val = 0;
+            do { c = readByte(); } while (c <= ' ' && c != -1);
+            if (c == '-') { sign = -1; c = readByte(); }
+            while (c > ' ' && c != -1) {
+                val = val * 10 + (c - '0');
+                c = readByte();
+            }
+            return val * sign;
+        }
+
+        String next() throws java.io.IOException {
+            int c;
+            do {
+                c = readByte();
+            } while (c <= ' ' && c != -1);
+
+            if (c == -1) return null;
+
+            StringBuilder sb = new StringBuilder();
+            while (c > ' ' && c != -1) {
+                sb.append((char) c);
+                c = readByte();
+            }
+            return sb.toString();
+        }
+    }
+
+    static FastScanner cin = new FastScanner(System.in);
+
+    public static <T extends Comparable<? super T>> boolean isSorted(List<T> list) {
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i-1).compareTo(list.get(i)) > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static long gcd(long a, long b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    public static long lcm(long a, long b) {
+        return a * b / gcd(a,b);
+    }
+
+    static long modPow(long base, int exp, int mod) {
+        long result = 1;
+        base %= mod;
+        while (exp > 0) {
+            if ((exp & 1) == 1)
+                result = result * base % mod;
+            base = base * base % mod;
+            exp >>= 1;
+        }
+        return result;
+    }
+
+    static final int N = 5001;
+    static double[][] P = new double[N][N];
+
+    static {
+        P[1][1] = 1.0;
+        P[2][1] = 0.5;
+
+        for (int k = 3; k < N; ++k) {
+            P[k][1] = 1.0 / k;
+            for (int i = 2; i <= k; ++i) {
+                P[k][i] = (P[k - 2][i - 2] * (i - 2) + P[k - 2][i - 1] * (k - i)) / k;
+            }
+        }
+    }
+
+    static void solve() throws IOException {
+        int n = cin.nextInt();
+        int m = cin.nextInt();
+
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
+
+        for (int i = 0; i < m; ++i) {
+            int a = cin.nextInt() - 1;
+            int b = cin.nextInt() - 1;
+            adj.get(a).add(b);
+        }
+
+        double[] dp = new double[n];
+        dp[n - 1] = 1.0;
+
+        for (int x = n - 2; x >= 0; --x) {
+            List<Integer> list = adj.get(x);
+            list.sort(Comparator.comparingDouble(i -> -dp[i]));
+            for (int i = 0; i < list.size(); ++i) {
+                dp[x] += dp[list.get(i)] * P[list.size()][i + 1];
+            }
+        }
+
+        System.out.printf("%.20f%n", dp[0]);
+    }
+
+    public static void main(String[] args) throws IOException {
+        int t = cin.nextInt();
+//        int t = 1;
+        while (t-- > 0) {
+            solve();
+        }
+    }
+}
