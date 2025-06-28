c7e7e4551304218f4335d2f9531d1842975bdb73
Alexander Garifullin <139747118+AlexanderGarifullin@users.noreply.github.com>
2025-06-28 12:47:17 +0500

    bitmasks, geometry, math, number theory

---
+++ Codeforces/Rounds/Div-1/736/1548D1.java
@@ -0,0 +1,133 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.BiConsumer;
+
+public class Main {
+    private static final class FastScanner {
+        private final byte[] buf = new byte[1 << 16];
+        private int len = 0, ptr = 0;
+        private final InputStream in;
+
+        FastScanner(InputStream in) {
+            this.in = in;
+        }
+
+        private int readByte() throws java.io.IOException {
+            if (ptr >= len) {
+                len = in.read(buf);
+                ptr = 0;
+                if (len <= 0) return -1;
+            }
+            return buf[ptr++];
+        }
+
+        int nextInt() throws java.io.IOException {
+            int c, sign = 1, val = 0;
+            do { c = readByte(); } while (c <= ' ' && c != -1);
+            if (c == '-') { sign = -1; c = readByte(); }
+            while (c > ' ' && c != -1) {
+                val = val * 10 + (c - '0');
+                c = readByte();
+            }
+            return val * sign;
+        }
+
+        long nextLong() throws java.io.IOException {
+            long c, sign = 1, val = 0;
+            do { c = readByte(); } while (c <= ' ' && c != -1);
+            if (c == '-') { sign = -1; c = readByte(); }
+            while (c > ' ' && c != -1) {
+                val = val * 10 + (c - '0');
+                c = readByte();
+            }
+            return val * sign;
+        }
+
+        String next() throws java.io.IOException {
+            int c;
+            do {
+                c = readByte();
+            } while (c <= ' ' && c != -1);
+
+            if (c == -1) return null;
+
+            StringBuilder sb = new StringBuilder();
+            while (c > ' ' && c != -1) {
+                sb.append((char) c);
+                c = readByte();
+            }
+            return sb.toString();
+        }
+    }
+
+    static FastScanner cin = new FastScanner(System.in);
+
+    public static <T extends Comparable<? super T>> boolean isSorted(List<T> list) {
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i-1).compareTo(list.get(i)) > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static long gcd(long a, long b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    public static long lcm(long a, long b) {
+        return a * b / gcd(a,b);
+    }
+
+    static long modPow(long base, int exp, int mod) {
+        long result = 1;
+        base %= mod;
+        while (exp > 0) {
+            if ((exp & 1) == 1)
+                result = result * base % mod;
+            base = base * base % mod;
+            exp >>= 1;
+        }
+        return result;
+    }
+
+    static void solve() throws IOException {
+        int n = cin.nextInt();
+        int[] x = new int[n];
+        int[] y = new int[n];
+        int cnt = 0;
+        int[][] t = new int[2][2];
+
+        for (int i = 0; i < n; i++) {
+            x[i] = cin.nextInt() / 2;
+            y[i] = cin.nextInt() / 2;
+            if (x[i] % 2 == y[i] % 2) cnt++;
+            t[x[i] % 2][y[i] % 2]++;
+        }
+
+        long ans = 0;
+        ans += (long) cnt * (cnt - 1) * (cnt - 2) / 6;
+        ans += (long) (n - cnt) * (n - cnt - 1) * (n - cnt - 2) / 6;
+
+        for (int xmod = 0; xmod < 2; xmod++) {
+            ans += (long) t[xmod][1] * (t[xmod][1] - 1) / 2 * t[xmod][0];
+            ans += (long) t[1][xmod] * (t[1][xmod] - 1) / 2 * t[0][xmod];
+            ans += (long) t[xmod][0] * (t[xmod][0] - 1) / 2 * t[xmod][1];
+            ans += (long) t[0][xmod] * (t[0][xmod] - 1) / 2 * t[1][xmod];
+        }
+
+        System.out.println(ans);
+    }
+
+    public static void main(String[] args) throws IOException {
+//        int t = cin.nextInt();
+        int t = 1;
+        while (t-- > 0) {
+            solve();
+        }
+    }
+}
