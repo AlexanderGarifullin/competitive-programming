e4b5b1c93fdb02be3734ebd909dead3ea8764da0
Alexander Garifullin <139747118+AlexanderGarifullin@users.noreply.github.com>
2025-07-06 13:01:42 +0500

    combinatorics, dp, math

---
+++ Codeforces/Rounds/Div-1+Div-2/463/932E.java
@@ -0,0 +1,132 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.BiConsumer;
+
+public class Main {
+    private static final class FastScanner {
+        private final byte[] buf = new byte[1 << 16];
+        private int len = 0, ptr = 0;
+        private final InputStream in;
+
+        FastScanner(InputStream in) {
+            this.in = in;
+        }
+
+        private int readByte() throws java.io.IOException {
+            if (ptr >= len) {
+                len = in.read(buf);
+                ptr = 0;
+                if (len <= 0) return -1;
+            }
+            return buf[ptr++];
+        }
+
+        int nextInt() throws java.io.IOException {
+            int c, sign = 1, val = 0;
+            do { c = readByte(); } while (c <= ' ' && c != -1);
+            if (c == '-') { sign = -1; c = readByte(); }
+            while (c > ' ' && c != -1) {
+                val = val * 10 + (c - '0');
+                c = readByte();
+            }
+            return val * sign;
+        }
+
+        long nextLong() throws java.io.IOException {
+            long c, sign = 1, val = 0;
+            do { c = readByte(); } while (c <= ' ' && c != -1);
+            if (c == '-') { sign = -1; c = readByte(); }
+            while (c > ' ' && c != -1) {
+                val = val * 10 + (c - '0');
+                c = readByte();
+            }
+            return val * sign;
+        }
+
+        String next() throws java.io.IOException {
+            int c;
+            do {
+                c = readByte();
+            } while (c <= ' ' && c != -1);
+
+            if (c == -1) return null;
+
+            StringBuilder sb = new StringBuilder();
+            while (c > ' ' && c != -1) {
+                sb.append((char) c);
+                c = readByte();
+            }
+            return sb.toString();
+        }
+    }
+
+    static FastScanner cin = new FastScanner(System.in);
+
+    public static <T extends Comparable<? super T>> boolean isSorted(List<T> list) {
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i-1).compareTo(list.get(i)) > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static long gcd(long a, long b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    public static long lcm(long a, long b) {
+        return a * b / gcd(a,b);
+    }
+
+    static long modPow(long base, int exp, int mod) {
+        long result = 1;
+        base %= mod;
+        while (exp > 0) {
+            if ((exp & 1) == 1)
+                result = result * base % mod;
+            base = base * base % mod;
+            exp >>= 1;
+        }
+        return result;
+    }
+
+    static final int MOD = 1_000_000_007;
+    static int[][] dp;
+
+    static int fill(int diffs, int a, int total) {
+        if (dp[diffs][a] != -1) return dp[diffs][a];
+        int b = total - a;
+        if (diffs == 0) {
+            return dp[diffs][a] = (int) modPow(2, b, MOD);
+        }
+        long res = 0;
+        if (b > 0) res += (long) b * fill(diffs - 1, a + 1, total);
+        if (a > 0) res += (long) a * fill(diffs - 1, a, total);
+        return dp[diffs][a] = (int) (res % MOD);
+    }
+
+    static void solve() throws IOException {
+        int n = cin.nextInt();
+        int k = cin.nextInt();
+        dp = new int[k + 1][k + 1];
+        for (int i = 0; i <= k; i++) {
+            for (int j = 0; j <= k; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        System.out.println(fill(k, 0, n));
+    }
+
+    public static void main(String[] args) throws IOException {
+//        int t = cin.nextInt();
+        int t = 1;
+        while (t-- > 0) {
+            solve();
+        }
+    }
+}
